8622fcb92a2f8b42cfa605e6fcde5ef3033deddf
Pintu kumar <pintu1012kumar@gmail.com>
2024-07-05 00:01:31 +0530

    build movies suggestion list

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import MovieList from './MovieList'
+import {useSelector} from "react-redux";
 
 const SecondaryContainer = () => {
+const movies = useSelector((store) => store.movies);
+
   return (
-    <div> 
-      SecondaryContainer
+  movies.nowPlayingMovies && (
+    <div className=' bg-black'> 
+
+    <div className='-mt-52  pl-12 relative z-20'>
+      <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/> 
+      <MovieList title={"Trending"} movies={movies.nowPlayingMovies}/> 
+      <MovieList title={"Popular"} movies={movies.nowPlayingMovies}/> 
+      <MovieList title={"Upcoming movies"} movies={movies.nowPlayingMovies}/> 
+      <MovieList title={"Horror"} movies={movies.nowPlayingMovies}/> 
+    </div>
+
+
+
+      {/* 
+         MovieList - Popular 
+          MovieCards*n
+         MovieList - Now playing  
+         MovieList - Trending 
+         MovieList - Horror 
+      */}
     </div>
   )
+);
 }
 
 export default SecondaryContainer
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,6 +11,8 @@ export const API_OPTIONS = {
   }
 };
 
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
 // export const API_OPTIONS = {
 //   method: 'GET',
 //   headers: {
